5b699ed51a98f85943526c2614f7974b64256e1d
Landrok <landrok@users.noreply.github.com>
2017-05-15 19:34:49 +0200

    [Installer] Fix Composer installation

--- bin/install.sh
+++ bin/install.sh
@@ -127,17 +127,13 @@ source "$WSI_BASEDIR/bin/install/apache2.sh"
 
 # Install composer
 echo "[INFO] Composer install"
-if [[ -z "${TRAVIS_PHP_VERSION+x}" ]]; then
-  # Classic install
-  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-  php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-  php composer-setup.php
-  php -r "unlink('composer-setup.php');"
-  su "$WSI_USER" -c "php composer.phar update --no-dev -o"
-else
-  # Travis CI
-  su "$WSI_USER" -c "composer update --no-dev -o"
-fi
+su "$WSI_USER"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar update --no-dev -o
+sudo su
 
 # Initialize data files
 echo "[INFO] Initializing data files"
