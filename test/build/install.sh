31486cc420b84e3187e7bca2368b0c8e3f43711f
Landrok <landrok@users.noreply.github.com>
2017-05-18 19:56:22 +0200

    [Test] Add success message for shellcheck installation

--- test/build/install.sh
+++ test/build/install.sh
@@ -7,8 +7,13 @@
 
 set -eo pipefail
 
+# shellcheck source=bin/lib/bash-utils.sh disable=1091
+source "bin/lib/bash-utils.sh"
+
 curl -Lso \
   /usr/bin/shellcheck \
   https://github.com/caarlos0/shellcheck-docker/releases/download/v0.4.6/shellcheck
 
 chmod +x /usr/bin/shellcheck
+
+success "shellcheck installed"
